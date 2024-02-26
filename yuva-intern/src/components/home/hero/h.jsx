8592777f1233912859af0f4e9e66376853d9a2b9
anujdubey88 <anujd1595@gmail.com>
2024-02-27 01:27:14 +0530

    this is basic layout

---
+++ yuva-intern/src/components/home/hero/h.jsx
@@ -0,0 +1,90 @@
+import React from "react";
+import "./h.css";
+
+export default function H() {
+  return (
+    <>
+    <h1 className="ourcourses">OUR COURSES</h1>
+    <div className="cards">
+      <div class="card data"  >
+        <img src="../images/logo/data-science.jpg" class="card-img-top" alt="..." />
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="/" class="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
+      <div class="card data" >
+        <img src="../images/logo/data-science.jpg" class="card-img-top" alt="..." />
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="/" class="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
+      <div class="card data" >
+        <img src="../images/logo/data-science.jpg" class="card-img-top" alt="..." />
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="/" class="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
+      <div class="card data">
+        <img src="../images/logo/data-science.jpg" class="card-img-top" alt="..." />
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="/" class="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
+      <div class="card data">
+        <img src="../images/logo/data-science.jpg" class="card-img-top" alt="..." />
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="/" class="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
+      <div class="card data">
+        <img src="../images/logo/data-science.jpg" class="card-img-top" alt="..." />
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="/" class="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
+    </div>
+    </>
+  );
+}
