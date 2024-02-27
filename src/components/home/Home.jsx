d9900f132ae68384de3dc44f8a774feb9c3ee1a0
anujdubey88 <anujd1595@gmail.com>
2024-02-27 22:43:32 +0530

    yuva intern

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,24 @@
+import React from "react"
+import AboutCard from "../about/AboutCard"
+// import Hblog from "./Hblog"
+// import HAbout from "./HAbout"
+import Hero from "./hero/Hero"
+// import Hprice from "./Hprice"
+// import Testimonal from "./testimonal/Testimonal"
+import H from "./hero/h"
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <AboutCard />
+      {/* <HAbout /> */}
+      <H/>
+      {/* <Testimonal /> */}
+      {/* <Hblog /> */}
+      {/* <Hprice /> */}
+    </>
+  )
+}
+
+export default Home
---
+++ src/components/home/hero/Hero.jsx
@@ -0,0 +1,32 @@
+import React from "react"
+import Heading from "../../common/heading/Heading"
+import "./Hero.css"
+
+const Hero = () => {
+  return (
+    <>
+      <section className='hero'>
+        <div className='container'>
+          <div className='row'>
+          <h3 style={{fontSize:"25px",paddingBottom:"30px"}}>WELCOME TO Yuva-Intern</h3>
+          <h1 style={{fontSize:"40px",paddingBottom:"10px"}}>Take Your Career to the Next Level with us</h1>
+            <p >If you are after a New and Exciting learning Experience that can
+                Help you reach your Goals and Unleash your full otential then
+                come and be our Intern</p>
+            <div className='button'>
+              <button className='primary-btn'>
+                GET STARTED NOW <i className='fa fa-long-arrow-alt-right'></i>
+              </button>
+              <button>
+                VIEW COURSE <i className='fa fa-long-arrow-alt-right'></i>
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+      <div className='margin'></div>
+    </>
+  )
+}
+
+export default Hero
